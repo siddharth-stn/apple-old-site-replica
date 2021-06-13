f3d496c3630b76ebff9499620e4476d30900b840
siddharth-stn <66431369+siddharth-stn@users.noreply.github.com>
2021-06-13 20:18:22 +0530

    Project Complete

---
+++ images/Apple_files/search_decorator.js
@@ -0,0 +1,37 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+Event.onDOMReady(function(){var h=document.getElementsByTagName("input");for(var c=0;c<h.length;c++){var S=$(h[c]);if(S.hasClassName("prettysearch")){var p={};if(S.hasClassName("applesearch")){p.autosave="Apple.com";}else{if(S.hasClassName("reseller")){p.autosave="Apple.com Reseller";}}if(S.parentNode.tagName=="LABEL"){var A="";var j=S.up().getElementsByClassName("prettyplaceholder")[0];if(j){A=j.innerHTML;}else{A=S.parentNode.firstChild.nodeValue;S.parentNode.firstChild.nodeValue="";}A=A.split("\n")[0];p.placeholder=A;}AC.decorateSearchInput(S,p);}}});
+
+
+}
+/*
+     FILE ARCHIVED ON 00:43:09 Mar 01, 2014 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 11:05:54 Jun 13, 2021.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 6131.016
+  load_resource: 3823.826 (2)
+  exclusion.robots.policy: 0.215
+  RedisCDXSource: 1.946
+  LoadShardBlock: 4648.293 (3)
+  exclusion.robots: 0.23
+  esindex: 0.05
+  PetaboxLoader3.datanode: 3546.109 (5)
+  PetaboxLoader3.resolve: 2137.104 (2)
+  CDXLines.iter: 128.027 (3)
+*/
---
+++ images/Apple_files/touch_forcedlink.js
@@ -0,0 +1,38 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+(function(){if(s&&Event&&AC&&typeof AC.onDOMReady==="function"&&Event.Listener&&typeof Event.Listener.listenForEvent==="function"&&typeof AC.ViewMaster==="object"&&typeof s==="object"){AC.onDOMReady(function(){Event.Listener.listenForEvent(AC.ViewMaster,"ViewMasterWillShowNotification",false,function(b){s.useForcedLinkTracking=false
+});Event.Listener.listenForEvent(AC.ViewMaster,"ViewMasterDidShowNotification",false,function(b){s.useForcedLinkTracking=true
+})})}})();
+
+}
+/*
+     FILE ARCHIVED ON 00:43:18 Mar 01, 2014 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 11:05:56 Jun 13, 2021.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  exclusion.robots.policy: 0.206
+  CDXLines.iter: 35.666 (3)
+  captures_list: 563.082
+  load_resource: 187.107 (2)
+  RedisCDXSource: 0.817
+  LoadShardBlock: 432.935 (3)
+  exclusion.robots: 0.22
+  PetaboxLoader3.datanode: 290.319 (5)
+  esindex: 0.014
+  PetaboxLoader3.resolve: 103.296 (2)
+*/
